baf993d2425b33a5798af3350f60a9fe2179cc34
Kerne1bug <i@pfedulov.ru>
2023-06-12 17:13:03 +0300

    11.all done

---
+++ js/11/11.8_task_1_0lvl.js
@@ -0,0 +1,29 @@
+/*Перепишите один из примеров раздела Цепочка промисов, используя async/await вместо .then/catch:*/
+
+function loadJson(url) {
+  return fetch(url)
+    .then(response => {
+      if (response.status == 200) {
+        return response.json();
+      } else {
+        throw new Error(response.status);
+      }
+    })
+}
+
+loadJson('no-such-user.json') // (3)
+  .catch(alert); // Error: 404
+
+
+//решение
+async function loadJson(url) {
+    const response = await fetch(url);
+    if (response.status === 200) {
+        return response.json();
+    } else {
+        throw new Error (response.status);
+    } 
+}
+
+loadJson('no-such-user.json')
+    .catch(alert);
